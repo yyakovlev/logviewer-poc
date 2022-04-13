fa94edd9a1837ebfcbebe6b5659526c3cbd242f8
yyakovlev <yury.yakovlev@gmail.com>
2022-04-13 17:19:01 +0300

    [Logviewer-POC] Finalize approach to change log file path dynamically in runtime via JMX

--- src/main/java/com/example/logviewerpoc/LogviewerPocApplication.java
+++ src/main/java/com/example/logviewerpoc/LogviewerPocApplication.java
@@ -1,6 +1,8 @@
 package com.example.logviewerpoc;
 
 import com.example.logviewerpoc.service.LogsGenerator;
+import com.logviewer.data2.LogFormat;
+import com.logviewer.logLibs.LogConfigurationLoader;
 import com.logviewer.springboot.LogViewerSpringBootConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +10,16 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @SpringBootApplication
 @Slf4j
@@ -20,6 +29,21 @@ public class LogviewerPocApplication {
 	public static void main(String[] args) {
 		System.setProperty("spring.profiles.active", "POC");
 		SpringApplication.run(LogviewerPocApplication.class, args);
+	}
+
+	@Bean
+	public LogConfigurationLoader logConfigurationLoader() {
+		return new LogConfigurationLoader() {
+			@Override
+			public Map<Path, LogFormat> getLogConfigurations() {
+				Map<Path , LogFormat> result = new LinkedHashMap<>();
+				File logDir = new File("./logs" );
+				for (File logFile : Objects.requireNonNull(logDir.listFiles())){
+					result.put(Paths.get(logFile.getAbsolutePath()) , null);
+				}
+				return result;
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/logviewerpoc/service/ActuatorLogPathManager.java
+++ src/main/java/com/example/logviewerpoc/service/ActuatorLogPathManager.java
@@ -1,6 +1,6 @@
 package com.example.logviewerpoc.service;
 
-import org.apache.commons.beanutils.BeanUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.logging.LogFileWebEndpoint;
@@ -9,7 +9,7 @@ import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
@@ -19,33 +19,54 @@ import java.nio.file.Paths;
 @Profile("POC")
 @ManagedResource
         //(description = "Set up path to log file for actuator exposure at /actuator/logfile")
+@Slf4j
 public class ActuatorLogPathManager {
 
-    @Autowired
+    @Autowired(required = false)
     private LogFileWebEndpoint logFileWebEndpoint;
 
+    private String originalLogFile;
+
+    @PostConstruct
+    public void init() throws IllegalAccessException {
+        originalLogFile = getExternalFile();
+        log.info("Saving original log file path: {}", originalLogFile);
+    }
+
     @ManagedOperation(description = "Set new log file path")
-    public String setLogFile(String logFile) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    public String setLogFile(String logFile) throws IllegalAccessException {
+        if (logFile == null) {
+            return "Cannot set log file path to null, skipping.";
+        }
+
         if (Files.exists(Paths.get(logFile))) {
-            String message = String.format("Replaced %s with %s as log file", getExternalFile(), logFile);
+            String message = String.format("Replaced %s with %s as default log file to view", getExternalFile(), logFile);
             setExternalFile(logFile);
             return message;
         } else {
-            return String.format("Path %s does not exists. Log file path remained as %s",
+            return String.format("Path %s does not exists. Path of default log file to view remained as %s",
                     logFile, getExternalFile());
         }
     }
 
-    @ManagedOperation(description = "Set new log file path")
-    public String getLogFile() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    @ManagedOperation(description = "Get active log file path")
+    public String getLogFile() throws IllegalAccessException {
         return getExternalFile();
     }
 
-    private void setExternalFile(String externalFile) throws InvocationTargetException, IllegalAccessException {
-        FieldUtils.writeField(logFileWebEndpoint, "externalFile", new File(externalFile));
+    @ManagedOperation(description = "Reset active log file path to original value")
+    public String reset() throws IllegalAccessException {
+        StringBuilder message = new StringBuilder("Reset log file from ").append(getExternalFile());
+        setLogFile(originalLogFile);
+        message.append(" to original value: ").append(originalLogFile);
+        return message.toString();
     }
 
-    private String getExternalFile() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    private void setExternalFile(String externalFile) throws IllegalAccessException {
+        FieldUtils.writeField(logFileWebEndpoint, "externalFile", new File(externalFile), true);
+    }
+
+    private String getExternalFile() throws IllegalAccessException {
         File externalFile = (File)FieldUtils.readField(logFileWebEndpoint, "externalFile", true);
         if (externalFile == null) {
             return null;
