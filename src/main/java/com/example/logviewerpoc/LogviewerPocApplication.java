ba64e049514d38410441fcc1b367b00859be4443
yyakovlev <yury.yakovlev@gmail.com>
2022-04-17 23:31:43 +0300

    [Logviewer-POC] supported 2 distinct log dirs

--- src/main/java/com/example/logviewerpoc/LogviewerPocApplication.java
+++ src/main/java/com/example/logviewerpoc/LogviewerPocApplication.java
@@ -3,9 +3,12 @@ package com.example.logviewerpoc;
 import com.example.logviewerpoc.service.LogsGenerator;
 import com.logviewer.data2.LogFormat;
 import com.logviewer.logLibs.LogConfigurationLoader;
+import com.logviewer.services.LvFileAccessManagerImpl;
+import com.logviewer.services.PathPattern;
 import com.logviewer.springboot.LogViewerSpringBootConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +23,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @Slf4j
@@ -37,13 +42,29 @@ public class LogviewerPocApplication {
 			@Override
 			public Map<Path, LogFormat> getLogConfigurations() {
 				Map<Path , LogFormat> result = new LinkedHashMap<>();
-				File logDir = new File("./logs" );
-				for (File logFile : Objects.requireNonNull(logDir.listFiles())){
-					result.put(Paths.get(logFile.getAbsolutePath()) , null);
-				}
+				addLogDir("logs/app", result);
+				addLogDir("logs/raw", result);
 				return result;
 			}
 		};
 	}
+
+	private void addLogDir(String relativeDir, Map<Path, LogFormat> result) {
+		File logDir = new File(relativeDir ).getAbsoluteFile();
+		for (File logFile : Objects.requireNonNull(logDir.listFiles())){
+			result.put(Paths.get(logFile.getAbsolutePath()) , null);
+		}
+	}
+
+//	@Bean
+//	public LvFileAccessManagerImpl lvLogManager(@Value("${log-viewer.accessible-files.pattern:}") List<String> accessiblePatterns) {
+//		LvFileAccessManagerImpl res = new LvFileAccessManagerImpl(null);
+//
+//		Stream<PathPattern> files = getLogFormats().keySet().stream().map(PathPattern::file);
+//		Stream<PathPattern> dirs = accessiblePatterns.stream().map(PathPattern::fromPattern);
+//
+//		res.setPaths(Stream.concat(files, dirs).collect(Collectors.toList()));
+//		return res;
+//	}
 
 }
--- src/main/java/com/example/logviewerpoc/service/LogsGenerator.java
+++ src/main/java/com/example/logviewerpoc/service/LogsGenerator.java
@@ -2,15 +2,20 @@ package com.example.logviewerpoc.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.mutable.MutableInt;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.slf4j.Logger;
+
 @Service
 @Slf4j
 public class LogsGenerator {
+    private static final Logger RAW = LoggerFactory.getLogger("RAW");
+
     private AtomicInteger logRecordCount = new AtomicInteger(0);
 
     public void generateLogs(int recordCount, int nThreads) {
@@ -20,7 +25,7 @@ public class LogsGenerator {
                es.submit(() -> {
                    while (logRecordCount.intValue() < recordCount) {
                        String message = "Current time: " + LocalDateTime.now();
-                       switch (logRecordCount.intValue() % 4) {
+                       switch (logRecordCount.intValue() % 5) {
                            case 0:
                              log.error(message);
                              break;
@@ -33,6 +38,8 @@ public class LogsGenerator {
                            case 3:
                                log.debug(message);
                                break;
+                           case 4:
+                               RAW.info(message);
                        }
 
                        if (logRecordCount.intValue() % 100 == 0) {
